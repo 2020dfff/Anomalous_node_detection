07a32f4edde2be26cf816a04f068d24cbf5c2066
2022dfff <1374490218@qq.com>
2023-02-24 16:35:57 +0800

    reorganize the code

--- Pre_processing/load_info.py
+++ Pre_processing/load_info.py
@@ -1,5 +1,5 @@
 """
-    Created on: 2023-01-09
+    Created on: 2023-01-10
     Author: Yang Fei
 """
 
@@ -7,6 +7,7 @@ import os
 import json
 import pickle
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 # from tqdm import tqdm
@@ -15,26 +16,26 @@ DST = "../data_demo"
 
 input_file = DST + "/input.txt"
 modified_file = DST + "/modified.txt"  # delete blank line
-output_file = DST + "/output.txt"  # re-organize the file
+unique_edges_file = DST + "/unique_edges_file.txt"  # re-organize the file, also make edge unique
+unique_pairs_file = DST + "/unique_pairs_file.txt"  # re-organize the file, also make edge unique
 reassign_file = DST + "/reassigned.txt"  # give the ip address new number
+log = open(DST + '/info.txt', mode='w', encoding='utf-8')
 
 
-# delete the blank line in the dataset to guarantee the function works
-def delete_blank_line(infile, modified):
+def preprocess(infile, outfile1, outfile2):
+    # delete the blank line in the dataset to guarantee the function works
+    edges = set()
+    pairs = set()
     with open(infile, "r") as file:
         lines = file.readlines()
+    lines = [line for line in lines if line.strip()]
+    with open(outfile1, "w") as f_edge_out, \
+         open(outfile2, "w") as f_pair_out:
+        f_edge_out.write("proto_name,srcip,dstip,srcport,dstport\n")
+        f_pair_out.write("proto_name,srcip,dstip,srcport,dstport\n")
 
-    with open(modified, "w+") as file:
+        # extract edges and pairs from the data
         for line in lines:
-            if line.strip():
-                file.write(line)
-
-
-def json_keyword():
-    delete_blank_line(input_file, modified_file)
-    with open(modified_file, "r") as f_in, open(output_file, "w+") as f_out:
-        f_out.write("proto_name,srcip,dstip,srcport,dstport\n")
-        for line in f_in:
             json_line = json.loads(line)
 
             proto_name = json_line.get("_source", {}).get("proto_name", "")
@@ -43,15 +44,26 @@ def json_keyword():
             srcport = json_line.get("_source", {}).get("srcport", "")
             dstport = json_line.get("_source", {}).get("dstport", "")
 
-            f_out.write(f"{proto_name},{srcip},{dstip},{srcport},{dstport}\n")
+            # add unique edges and pairs to sets
+            edge = (srcip, dstip, srcport, dstport)
+            pair = (srcip, dstip)
+            if edge not in edges:
+                f_edge_out.write(f"{proto_name},{srcip},{dstip},{srcport},{dstport}\n")
+                edges.add(edge)
+
+            if pair not in pairs:
+                f_pair_out.write(f"{proto_name},{srcip},{dstip},{srcport},{dstport}\n")
+                pairs.add(pair)
+
+    return edges, pairs
 
 
 def split():
     # do the initialization job to pick out key information
-    json_keyword()
+    preprocess(input_file, unique_edges_file, unique_pairs_file,)
 
     # Re-assign the ip with number
-    f_in = open(output_file, 'r')
+    f_in = open(unique_pairs_file, 'r')
     f_out = open(reassign_file, 'w+')  # + str(cur_time) + '.txt'
 
     line = f_in.readline()  # Skip headers
@@ -88,56 +100,61 @@ def split():
 
     with open(DST + '/nmap.pkl', 'wb+') as f:
         pickle.dump(nmap_rev, f, protocol=pickle.HIGHEST_PROTOCOL)
+    # print(nmap_rev[3])  # output: 10.3.27.203
 
-    # Calculate some key information about the data
-    def calculate(target_file):
-        df = pd.read_csv(target_file, header=None)
-        # Basic information about the file
-        print("The name of this file is:", str(target_file))
-        print("=====================================================================")
-        total_edge = 0
-        total_node = 0
-        edge_without_port = 0
-        with open(target_file, 'r') as tmp:
-            for edge in tmp:
-                total_edge += 1
-                columns = edge.split(',')
-                if len(columns) >= 3:
-                    number_2 = int(columns[1])
-                    number_3 = int(columns[2])
-                    if number_2 > total_node:
-                        total_node = number_2
-                    if number_3 > total_node:
-                        total_node = number_3
-                    if (columns[3] == "") or (columns[4] == ""):
-                        edge_without_port += 1
-        print("Total number of edges:", total_edge)
-        print("Total number of nodes:", total_node)
-        total_port = len(set(df[3].tolist() + df[4].tolist()))
-        print("Total number of ports:", total_port)
-        print("=====================================================================")
-        normal_percent = (1-edge_without_port/total_edge)*100
-        print('Normal edges percent: %f%%:' % normal_percent)
-        print("=====================================================================")
+    calculate(reassign_file, nmap_rev)
 
-        # calculate the Node egress and ingress
-        counts_in = df[1].value_counts()
-        counts_out = df[2].value_counts()
 
-        # Print the top 10 node with the highest ingress
-        top_in_nodes = counts_in.head(10).index.tolist()
-        top_in_ip = [nmap_rev[node] for node in top_in_nodes]
-        print("The top ten IPs with the largest ingress:\n", top_in_ip)
-        print("=====================================================================")
+# Calculate some key information about the data
+def calculate(target_file, nmap_rev):
+    df = pd.read_csv(target_file, header=None)
+    # Basic information about the file
+    print("The name of this file is: %s\n=====================================================================" % str(target_file), file=log)
+    total_edge = 0
+    total_node = 0
+    edge_without_port = 0
+    self_loop = 0
+    with open(target_file, 'r') as tmp:
+        for edge in tmp:
+            total_edge += 1
+            columns = edge.split(',')
+            if len(columns) >= 3:
+                number_2 = int(columns[1])
+                number_3 = int(columns[2])
+                if number_2 > total_node:
+                    total_node = number_2
+                if number_3 > total_node:
+                    total_node = number_3
+                if (columns[3] == "") or (columns[4] == ""):
+                    edge_without_port += 1
+                if columns[1] == columns[2]:
+                    self_loop += 1
+    print("Total number of edges: %s\n=====================================================================" % total_edge, file=log)
+    print("Total number of nodes: %s\n=====================================================================" % total_node, file=log)
+    total_port = len(set(df[3].tolist() + df[4].tolist()))
+    print("Total number of ports: %s\n=====================================================================" % total_port, file=log)
+    normal_percent = (1-edge_without_port/total_edge)*100
+    print('Normal edges percent: %f%%\n=====================================================================' % normal_percent, file=log)
+    print("Number of self-loop is: %f\n=====================================================================" % self_loop, file=log)
 
-        # Print the top 10 node with the highest egress
-        top_out_nodes = counts_out.head(10).index.tolist()
-        top_out_ip = [nmap_rev[node] for node in top_out_nodes]
-        print("The top ten IPs with the largest egress:\n", top_out_ip)
-        print("=====================================================================")
+    # calculate the Node egress and ingress
+    counts_in = df[1].value_counts()
+    counts_out = df[2].value_counts()
 
-    calculate(reassign_file)
+    # Print the top 10 node with the highest ingress
+    top_in_nodes = counts_in.head(10).index.tolist()
+    top_in_ip = [nmap_rev[node] for node in top_in_nodes]
+    print("The top ten IPs with the largest ingress:\n", top_in_ip, file=log)
+    print("=====================================================================", file=log)
+
+    # Print the top 10 node with the highest egress
+    top_out_nodes = counts_out.head(10).index.tolist()
+    top_out_ip = [nmap_rev[node] for node in top_out_nodes]
+    print("The top ten IPs with the largest egress:\n", top_out_ip, file=log)
+    print("=====================================================================", file=log)
+    print("done!")
 
 
 if __name__ == '__main__':
     split()
+    log.close()
