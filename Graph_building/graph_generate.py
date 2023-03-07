77a7f47237e710794bfe3977b726d83ebcce1a4a
2022dfff <1374490218@qq.com>
2023-03-07 17:53:46 +0800

    update graph generating

--- Graph_building/graph_generate.py
+++ Graph_building/graph_generate.py
@@ -2,7 +2,7 @@ import networkx as nx
 # from Pre_processing import load_info
 # import pandas as pd
 import csv
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 unique_edges_DST = "../data_demo/unique_edges"
 unique_pairs_DST = "../data_demo/unique_pairs"
@@ -25,17 +25,37 @@ with open(edges_input, newline='') as f:
         edges.append((src, dst))
 
 # Print out edges list
-print(edges)
+# print(edges)
 
-# create a new graph
-G = nx.Graph()
+# create a new directed graph
+DG = nx.DiGraph()
 
+# build the graph with edges
 for edge in edges:
     src, dst = edge
-    G.add_node(src)
-    G.add_node(dst)
-    G.add_edge(src, dst)
+    DG.add_node(src)
+    DG.add_node(dst)
+    DG.add_edge(src, dst)
+G = DG.to_undirected(DG)
+print("graph building successful")
+
+# basic info about the graph
+is_weighted = nx.is_weighted(DG)
+is_directed = nx.is_directed(DG)
+print("is_weighted:", is_weighted, ", is_directed:", is_directed)
+
+# get top 10 nodes by degree centrality
+print("Top 10 nodes by degree centrality\n")
+dc = nx.degree_centrality(DG)
+top_nodes = sorted(dc.items(), key=lambda x: x[1], reverse=True)[:10]
+for node, dc in top_nodes:
+    print(f"{node}: {dc}")
+
+# Remove nodes with degree less than x
+low_degree_nodes = [node for node, degree in dict(DG.degree()).items() if degree < 300]
+DG.remove_nodes_from(low_degree_nodes)
 
 # draw the graph
-nx.draw(G, with_labels=True)
-
+nx.draw(DG, with_labels=True)
+plt.savefig("path.png")
+print("done")
